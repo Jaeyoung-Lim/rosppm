9f491a805b83ddd94801f4420463973acb55039c
gokoreas <gokoreas@gmail.com>
2016-02-21 16:49:42 +0900

    Implmentation of ppm read function
    - Implementation of ppm read
    #1

--- rosppm_arduino/ppm_signal.ino
+++ rosppm_arduino/ppm_signal.ino
@@ -1,8 +1,22 @@
 void set_ppm(){
     //Channel 1 to Channel 4 receives command values for control
     //minimum 650 max 1700
+    
     for(int i=0; i<number_of_outputs; i++){
-    pulses[0] = (int) cmd_val[0]*512.0+1175.0;
+    pulses[0] = (int) cmd_val[0]*512.0+1175.0; //Encode stick inputs into pulse values
     }
     
 }
+
+void read_ppm(){
+    // Read RX values
+  for (int i = 1; i < 7; i++){                                        //for each of the 6 channels:
+     in_pulses[i] = pulseIn(ppmin_PIN, HIGH, 20000);                            //read the receiver signal
+     
+     if (in_pulses[i] == 0) {in_pulses[i] = buff_pulses[i];} else {buff_pulses[i] = in_pulses[i];}    //if the signal is good then use it, else use the previous signal
+     
+     read_fval[i] = (float) in_pulses[i];
+     read_val[i] = (read_fval[i]-1175.0)/512.0;
+     //constrain (in_pulses[i], 0, 511);                                       //make sure that the value stays within the disired boundries
+  }
+ }
